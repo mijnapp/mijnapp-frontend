3ed2bd8f34dfa16c25c4fa2ee285e201ff004c37
E Versteeg <e.versteeg@solviteers.nl>
2019-08-29 10:00:54 +0200

    #9 sorteer adres

--- MijnApp-Frontend/src/redux/sagas/address.js
+++ MijnApp-Frontend/src/redux/sagas/address.js
@@ -16,18 +16,20 @@ function* fetchAddressData(action) {
     const result = yield call(addressApi.address(action, xAuth()));
     const address = JSON.parse(result.data).adressen;
     if (address.length > 1) {
-      address.sort(function (a, b) {
-        if (a.huisnummertoevoeging < b.huisnummertoevoeging) {
-          return -1;
-        }
-        if (a.huisnummertoevoeging > b.huisnummertoevoeging) {
-          return 1;
-        }
-        return 0;
-      });
+      address.sort(sortAddress);
     }
     yield put(requestAddressSuccess(address));
   } catch (e) {
     yield put(requestAddressDataFailure(e));
   }
+}
+
+function sortAddress(a, b) {
+  if (a.huisnummertoevoeging < b.huisnummertoevoeging) {
+    return -1;
+  }
+  if (a.huisnummertoevoeging > b.huisnummertoevoeging) {
+    return 1;
+  }
+  return 0;
 }
