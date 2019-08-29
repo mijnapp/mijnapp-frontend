46c9dc6cf479edb596d6bae210368b8ad954c944
E Versteeg <e.versteeg@solviteers.nl>
2019-08-29 16:14:10 +0200

    Roep processen api aan

---
+++ MijnApp-Backend/Controllers/ProcessController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace MijnApp_Backend.Controllers
+{
+    //[Authorize]
+    public class ProcessController : Controller
+    {
+        private const string URL_GETALL = "http://processes.zaakonline.nl/processes";
+        private const string URL_GETBYID = "http://processes.zaakonline.nl/processes/{0}";
+
+        [HttpGet]
+        [Route("processes")]
+        public async Task<IActionResult> Processes()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync(URL_GETALL);                
+                var result = await response.Content.ReadAsStringAsync();
+                return Json(result);
+            }
+        }
+
+        [HttpGet]
+        [Route("processes/{id}")]
+        public async Task<IActionResult> GetProcessesById(string id)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync(string.Format(URL_GETBYID, id));
+                var result = await response.Content.ReadAsStringAsync();
+                return Json(result);
+            }
+        }
+    }
+}
