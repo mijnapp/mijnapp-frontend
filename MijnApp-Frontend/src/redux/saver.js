c6d024316a9b0d31e0dd558c6013eb433ee1d44d
E Versteeg <e.versteeg@solviteers.nl>
2019-09-05 14:04:10 +0200

    #9 fix eslint warnings

--- MijnApp-Frontend/src/redux/saver.js
+++ MijnApp-Frontend/src/redux/saver.js
@@ -1,10 +1,10 @@
 export const saveState = (state) => {
-  let stringifiedState = JSON.stringify(state);
+  const stringifiedState = JSON.stringify(state);
   localStorage.setItem('appState', stringifiedState);
 };
 export const loadState = () => {
-  let json = localStorage.getItem('appState') || '{}';
-  let state = JSON.parse(json);
+  const json = localStorage.getItem('appState') || '{}';
+  const state = JSON.parse(json);
 
   if (state) {
     return state;
