6bd4f00a0a1ee30318ee7679f2f0251b1db53e31
E Versteeg <e.versteeg@solviteers.nl>
2019-09-17 10:06:20 +0200

    #9 Filter status Niet gerealiseerd

--- MijnApp-Frontend/src/components/lib/playback-screens/playback-screen-address/index.js
+++ MijnApp-Frontend/src/components/lib/playback-screens/playback-screen-address/index.js
@@ -130,7 +130,8 @@ export default class PlaybackScreenAddress extends connect(store)(PolymerElement
   }
 
   filterAddresses(address) {
-    if (address.status_verblijfsobject === 'VerblijfsobjectIngetrokken') {
+    if (address.status_verblijfsobject === 'VerblijfsobjectIngetrokken'
+      || address.status_verblijfsobject === 'NietGerealiseerdVerblijfsobject') {
       return false;
     }
 
