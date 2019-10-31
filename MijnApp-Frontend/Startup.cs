bdfba7176daca379023ea5082503c598cd6fc34d
E Versteeg <e.versteeg@solviteers.nl>
2019-10-31 11:23:23 +0100

    Default Error page

--- MijnApp-Frontend/Startup.cs
+++ MijnApp-Frontend/Startup.cs
@@ -4,6 +4,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,7 +25,6 @@ namespace MijnApp_Frontend
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
         } 
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,8 +43,8 @@ namespace MijnApp_Frontend
 
             app.UseDefaultFiles();
             app.UseStaticFiles();
+            app.UseStatusCodePagesWithRedirects("/index.html");
             app.UseStatusCodePagesWithReExecute("/index.html");
-
         }
 
         private void ConfigureFrontend()
