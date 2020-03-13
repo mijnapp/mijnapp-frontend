90e6b861d20ffc6670168afc418546e3731686a7
Sjoerd Kuperus <s.kuperus@solviteers.nl>
2020-03-13 11:22:07 +0100

    Bug #42775, Task #42899: Nieuwe versie van verzoeken api implementeren. Dev apis verzoek werkend.

--- MijnApp-Backend/Startup.cs
+++ MijnApp-Backend/Startup.cs
@@ -67,6 +67,7 @@ namespace MijnApp_Backend
                 var processId = frontEndHeaders["X-NLX-Request-Process-Id"].ToString();
                 httpClient.DefaultRequestHeaders.Add("X-NLX-Request-Process-Id", processId);
             }
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(Configuration["Api:ApiKey"]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
