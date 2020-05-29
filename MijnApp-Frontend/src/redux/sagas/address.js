297407db5760930c1c2774f5cc58f911078f44c8
Sjoerd Kuperus <s.kuperus@solviteers.nl>
2020-05-29 10:16:37 +0200

    Maak slim huisnummer toevoeging sorteer algoritme.

--- MijnApp-Frontend/src/redux/sagas/address.js
+++ MijnApp-Frontend/src/redux/sagas/address.js
@@ -25,10 +25,49 @@ function* fetchAddressData(action) {
 }
 
 function sortAddress(a, b) {
-  if (a.huisnummertoevoeging < b.huisnummertoevoeging) {
+  // huisnummertoevoeging is usually empty or sometimes an "A" or "Bis".
+  // But in complex cases it can be "H 123" or "ZW"
+  // The sorting should first see if there are any numbers inside the huisnummertoevoeging.
+  // It should remove the numbers. Then do the sorting on alphabet.
+  // After it should sort on numbers. This means that ("B 4", "A 111", "A 70") should be sorted as A 70, A 111, B 4;
+  if (a.huisnummertoevoeging == undefined && b.huisnummertoevoeging == undefined) {
+    return 0;
+  }
+  if (a.huisnummertoevoeging == undefined && b.huisnummertoevoeging != undefined) {
     return -1;
   }
-  if (a.huisnummertoevoeging > b.huisnummertoevoeging) {
+  if (a.huisnummertoevoeging != undefined && b.huisnummertoevoeging == undefined) {
+    return 1;
+  }
+
+  var aHuisnummertoevoegingLetters = a.huisnummertoevoeging.match(/[a-zA-Z]+/g);
+  var bHuisnummertoevoegingLetters = b.huisnummertoevoeging.match(/[a-zA-Z]+/g);
+  if (aHuisnummertoevoegingLetters[0] < bHuisnummertoevoegingLetters[0]) {
+    return -1;
+  }
+  if (aHuisnummertoevoegingLetters[0] > bHuisnummertoevoegingLetters[0]) {
+    return 1;
+  }
+  // If the letters are the same between items. Check on numbers, first check if there are any numbers.
+  var aHuisnummertoevoegingNumbers = a.huisnummertoevoeging.match(/\d+/g);
+  var bHuisnummertoevoegingNumbers = b.huisnummertoevoeging.match(/\d+/g);
+
+  if (aHuisnummertoevoegingNumbers == null && bHuisnummertoevoegingNumbers == null) {
+    return 0;
+  }
+  if (aHuisnummertoevoegingNumbers == null && bHuisnummertoevoegingNumbers != null) {
+    return -1;
+  }
+  if (aHuisnummertoevoegingNumbers != null && bHuisnummertoevoegingNumbers == null) {
+    return 1;
+  }
+
+  var aNumber = parseInt(aHuisnummertoevoegingNumbers[0]);
+  var bNumber = parseInt(bHuisnummertoevoegingNumbers[0]);
+  if (aNumber < bNumber) {
+    return -1;
+  }
+  if (aNumber > bNumber) {
     return 1;
   }
   return 0;
