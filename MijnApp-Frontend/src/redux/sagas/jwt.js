8d95572e4c923a4abeba6d7688ea90310b2634fb
Sjoerd Kuperus <s.kuperus@solviteers.nl>
2020-06-29 11:49:49 +0200

    Feature #44988, Task #45021: return url na uitloggen instelbaar maken.

--- MijnApp-Frontend/src/redux/sagas/jwt.js
+++ MijnApp-Frontend/src/redux/sagas/jwt.js
@@ -87,16 +87,19 @@ export function* watchRequestJwtLogout() {
   yield takeLatest(REQUEST_JWT_LOGOUT, doJwtLogout);
 }
 
-function* doJwtLogout() {
+function* doJwtLogout(action) {
   yield call(jwtApi.logout(jwtBearerToken()));
   yield put(requestJwtLogoutSuccess());
   removeJwtBearerToken();
   removeLastAction();
   window.successToast.text = 'Succesvol uitgelogd';
   window.successToast.open();
-  //yield put(selectPage('signin'));
-  var url = window.location.origin;
-  window.location = url + '/startjourney?name=verhuizen';
+  if (action !== null && action !== undefined && action.returnUrl !== undefined && action.returnUrl !== null && action.returnUrl !== '') {
+    window.location = action.returnUrl;
+  } else {
+    var url = window.location.origin;
+    window.location = url + '/startjourney?name=verhuizen';
+  }
 }
 
 export function* watchRequestJwtLogout401() {
