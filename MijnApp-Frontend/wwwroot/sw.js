95664777ede46cf16c53a25fac8a6b371f720442
Sjoerd Kuperus <s.kuperus@solviteers.nl>
2020-06-30 10:46:51 +0200

    Toevoegen handmatig versienummer + handmatig draaien van build:prod npm task.

--- MijnApp-Frontend/wwwroot/sw.js
+++ MijnApp-Frontend/wwwroot/sw.js
@@ -15,7 +15,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox
 
 importScripts(
   "custom-sw.js",
-  "precache-manifest.9aa9dedbe4221b2acd3d29b7e7a035e8.js"
+  "precache-manifest.10ba9d165dadc0bac5e3107807c98eda.js"
 );
 
 workbox.core.skipWaiting();
